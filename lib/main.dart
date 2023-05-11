d190fa68b4267b0ac03cd2e25e9a0513f3335346
Jayesh Patil <jp7470484@gmail.com>
2023-05-11 16:21:44 +0530

    get set go

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Home',
+      title: 'Stock HIVE',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.grey,
